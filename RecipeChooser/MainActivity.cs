133db3ec9482c7485ccf30bd2be1ca0262422d9b
ensallad <joohan91@hotmail.com>
2017-11-16 20:04:25 +0100

    Försöke lösa bildstorleksproblemet, fixade några småbuggar i receptlistan

--- RecipeChooser/MainActivity.cs
+++ RecipeChooser/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.Content;
 
 namespace RecipeChooser
 {
-    [Activity(Label = "RecipeChooser", MainLauncher = true, Icon = "@drawable/startbild")]
+    [Activity(Label = "Receptväljaren", MainLauncher = true, Icon = "@drawable/startbild")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -41,16 +41,19 @@ namespace RecipeChooser
             recipeList.Add(new Recipes { Name = "Pizza", imagePath = "pizza", webLink = "https://www.koket.se/niclas_wahlgren/varmratter/mjol/akta_hemlagad_pizza/" });
             recipeList.Add(new Recipes { Name = "Chili con carne", imagePath = "Chili_con_carne", webLink = "https://www.koket.se/mat-for-stora-och-sma/charlotta-malhotra/chili-con-carne/" });
             recipeList.Add(new Recipes { Name = "Spaghetti med köttfärssås", imagePath = "spaghettibolognese", webLink = "https://www.koket.se/mitt-kok/jennie-wallden/klassisk-kottfarssas-med-spaghetti/" });
-            recipeList.Add(new Recipes { Name = "Carbonara", imagePath = "Carbonara", webLink = "https://www.koket.se/mitt-kok/paolo-roberto/carbonara-paolo-robertos-recept" });
+
+            recipeList.Add(new Recipes { Name = "Pasta carbonara", imagePath = "Carbonara", webLink = "https://www.koket.se/mitt-kok/paolo-roberto/carbonara-paolo-robertos-recept" });
             recipeList.Add(new Recipes { Name = "Pannkaka", imagePath = "Pannkaka", webLink = "https://www.koket.se/pannkaka-2" });
             recipeList.Add(new Recipes { Name = "Biff Stroganoff", imagePath = "Biffstroganoff", webLink = "https://www.koket.se/mitt-kok/tommy-myllymaki/biff-stroganoff/" });
             recipeList.Add(new Recipes { Name = "Kycklingkebab med pitabröd", imagePath = "Kycklingkebab", webLink = "https://www.koket.se/kycklingkebab-med-pitabrod" });
             recipeList.Add(new Recipes { Name = "Vietnamesiska sommarrullar", imagePath = "Varrullar", webLink = "https://www.koket.se/nyhetsmorgon/jennie-wallden/vietnamesiska-sommarrullar/" });
+
             recipeList.Add(new Recipes { Name = "Kåldolmar", imagePath = "Kaldolmar", webLink = "https://www.koket.se/matgladje-hela-livet/leif-mannerstrom/kaldolmar/" });
             recipeList.Add(new Recipes { Name = "Falafel", imagePath = "Falafel", webLink = "https://www.koket.se/falafel-med-myntasas" });
             recipeList.Add(new Recipes { Name = "Fish and chips", imagePath = "Fishandchips", webLink = "https://www.koket.se/marcus_samuelsson/varmratter/fisk_och_skaldjur/fish_and_chips/" });
             recipeList.Add(new Recipes { Name = "Pasta kyckling Alfredo", imagePath = "Pastaalfredo", webLink = "https://www.koket.se/one-pot-kyckling-alfredo" });
             recipeList.Add(new Recipes { Name = "Pyttipanna", imagePath = "Pyttipanna", webLink = "https://www.koket.se/per_morberg/varmratter/kott/per_morbergs_pyttipanna/" });
+
             recipeList.Add(new Recipes { Name = "Köttbullar med potatis och gräddsås", imagePath = "Kottbullarpotatis", webLink = "https://www.koket.se/hemlagade-kottbullar-med-graddsas" });
             recipeList.Add(new Recipes { Name = "Hamburgare", imagePath = "Hamburgare", webLink = "https://www.koket.se/mitt-kok/tommy-myllymaki/tommy-myllymakis-saftiga-cheeseburgare/" });
             recipeList.Add(new Recipes { Name = "Wookade nudlar med grönsaker", imagePath = "Wookadenudlar", webLink = "https://www.koket.se/koket/pelle-johansson/wokade-nudlar-med-gronsaker/" });
@@ -75,10 +78,11 @@ namespace RecipeChooser
             { imageDialog.SetImageResource(Resource.Drawable.pizza); }
             else if (imageName == "Chili_con_carne")
             { imageDialog.SetImageResource(Resource.Drawable.Chili_con_carne); }
+            else if (imageName == "spaghettibolognese")
+            { imageDialog.SetImageResource(Resource.Drawable.spaghettibolognese); }
+
             else if (imageName == "Carbonara")
-            { imageDialog.SetImageResource(Resource.Drawable.Carbonara); }
-            else if (imageName == "Chili_con_carne")
-            { imageDialog.SetImageResource(Resource.Drawable.Chili_con_carne); }
+            { imageDialog.SetImageResource(Resource.Drawable.Carbonara); }           
             else if (imageName == "Pannkaka")
             { imageDialog.SetImageResource(Resource.Drawable.Pannkaka); }
             else if (imageName == "Biffstroganoff")
@@ -87,6 +91,7 @@ namespace RecipeChooser
             { imageDialog.SetImageResource(Resource.Drawable.Kycklingkebab); }
             else if (imageName == "Varrullar")
             { imageDialog.SetImageResource(Resource.Drawable.Varrullar); }
+
             else if (imageName == "Kaldolmar")
             { imageDialog.SetImageResource(Resource.Drawable.Kaldolmar); }
             else if (imageName == "Falafel")
@@ -97,6 +102,7 @@ namespace RecipeChooser
             { imageDialog.SetImageResource(Resource.Drawable.Pastaalfredo); }
             else if (imageName == "Pyttipanna")
             { imageDialog.SetImageResource(Resource.Drawable.Pyttipanna); }
+
             else if (imageName == "Kottbullarpotatis")
             { imageDialog.SetImageResource(Resource.Drawable.Kottbullarpotatis); }
             else if (imageName == "Hamburgare")
